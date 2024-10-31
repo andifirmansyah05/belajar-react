c0a7227e09137c157204cab257c0751f96c92d0d
Andi Firmansyah <andfirsyah58@gmail.com>
2024-10-31 21:21:08 +0700

    buat repo baru belajar React

---
+++ 1-pendahuluan/script.js
@@ -0,0 +1,7 @@
+
+const app = document.querySelector('#app');
+const header = document.createElement('h1');
+const headerContent = document.createTextNode('Belajar React bareng WPU 🚀')
+header.appendChild(headerContent);
+// console.log(header);
+app.appendChild(header);
