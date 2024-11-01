1ae5a3c42f02293c953719cfdec81ebfb1ef481c
Andi Firmansyah <andfirsyah58@gmail.com>
2024-11-01 09:47:12 +0700

    pindahin latihan 1 ke latihan 2, dari cdn ke react vite langsung

--- 2-installasi-dan-konfigurasi/src/App.jsx
+++ 2-installasi-dan-konfigurasi/src/App.jsx
@@ -1,5 +1,27 @@
 import { useState } from "react";
+import Header from "./components/Header";
 
-function App() {}
+
+function App() {
+  const students = ["andi", "dika", "parno"];
+  const [likes, setLikes] = useState(0);
+  // console.log(React.useState(0));
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  return (
+    <>
+      <Header />
+      <Header name="Andi" />
+      <ul>
+        {students.map((student) => (
+          <li key={student}>{student}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </>
+  );
+}
 
 export default App;
---
+++ 2-installasi-dan-konfigurasi/src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header({ name }) {
+    return <h1>Belajar React bareng {name ? name : "WPU"} 🚀</h1>;
+  }
